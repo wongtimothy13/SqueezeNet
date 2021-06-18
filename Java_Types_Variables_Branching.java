ad07bb0a85c7bc07d4be14053d9550a7bc6548ba
Tim <wongtimothy13@gmail.com>
2021-06-18 16:42:32 -0500

    First Test

---
+++ Java_Types_Variables_Branching.java
@@ -0,0 +1,10 @@
+public class Java_Types_Variables_Branching {
+    public static void main(String[] args) {
+      double myDouble = 9.78;
+      int myInt = (int) myDouble; // Explicit casting: double to int
+  
+      System.out.println(myDouble);
+      System.out.println(myInt);
+    }
+  }
+  
