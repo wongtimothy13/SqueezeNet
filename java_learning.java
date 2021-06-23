03740f5c1630834979bca8dbdbd481c1b1084fb7
Tim <wongtimothy13@gmail.com>
2021-06-22 20:01:41 -0500

    update

--- java_learning.java
+++ java_learning.java
@@ -1,34 +1,34 @@
 import java.util.*;
+
 public class java_learning {
-    public static void main(String[] args){
-        // System.out.println("Hello World");
-        String[] arr = {"one","two","three","four"};
-        // System.out.println(Arrays.toString(arr));
-        // System.out.println(Arrays.toString(arr));
-        // System.out.println(arr[0]);
+    public static void main(String[] args) {
+        // System.out.println("Hey!");
+        Scanner sc = new Scanner(System.in);
+        // String temp = sc.nextLine();
+        // System.out.println(temp);
 
-        ArrayList<String> names = new ArrayList<String>(5);
-        // System.out.println(names);
-        names.add("BOB");
-        names.add("SARAH");
-        names.add("John");
-        // names.remove("BOB");
-        Collections.reverse(names);
-        // System.out.println(names);
-        // System.out.println(names.length());
-        // System.out.println(names.contains("Bob"));
-        // System.out.println(names.size()-2);
-
-
-        enum Sample{
-            ONE,
-            TWO,
-            THREE
-        }
         
-        for (Sample sam : Sample.values()) {
-            System.out.println(sam);
+        ArrayList<Integer> arL = new ArrayList<Integer>();
+        arL.add(145);
+        arL.add(23);
+        arL.add(26);
+        arL.add(345);
+        arL.add(235);
+        Collections.sort(arL);
+
+        final double startTime = System.currentTimeMillis();
+        int Sum = 0;
+        for (int i=0; i<1_000_000_000; i++){
+            Sum += i;
         }
+        final double endTime = System.currentTimeMillis();
+
+
+        // Collections.sort(arL);
+        // System.out.println(arL);
+        System.out.println(Sum);
+        System.out.println((double)((endTime - startTime)/1000));
+                
 
     }
 }
